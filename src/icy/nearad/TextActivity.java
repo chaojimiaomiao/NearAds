06df1d4cb44df7d7b582119d5fdbb1932686a555
zhaibingjie <zhiabingjie@baixing.com>
2014-03-10 11:21:19 +0800

    nearad

---
+++ src/icy/nearad/TextActivity.java
@@ -0,0 +1,178 @@
+package icy.nearad;
+
+import icy.baixing.entity.Ad;
+import icy.baixing.entity.Ad.EDATAKEYS;
+import icy.nearad.TextFactory.ViewWithDegree;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.hardware.Camera;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.RelativeLayout.LayoutParams;
+
+public class TextActivity extends Activity implements SurfaceHolder.Callback, SensorEventListener {
+	private RelativeLayout textsParent;
+	private TextFactory factory;
+	private ArrayList<Ad> adList;
+	private ArrayList<View> views;
+	private ArrayList<TextView> textViews;//按远近排过序，为了画得遮挡先后顺序
+	private ArrayList<Double> degreeList;//与textview按顺序距离对应
+	double lastDirection;
+	SensorManager sensorManager;
+	Sensor sensor;
+	
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.camera_textlayout);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);  //隐藏手机状态栏
+        
+        SurfaceView surfaceView = (SurfaceView) findViewById(R.id.cameraSurfaceView);
+        SurfaceHolder holder = surfaceView.getHolder();
+		holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		holder.addCallback(this);
+        
+        dHeight = getWindowManager().getDefaultDisplay().getHeight();
+        dWidth = getWindowManager().getDefaultDisplay().getWidth();
+        textsParent = (RelativeLayout)findViewById(R.id.textsParent);
+        adList = SplashCover.getAdsList();
+        factory = new TextFactory(this);
+        ArrayList<ViewWithDegree> lists= factory.setAds(adList);
+        views = new ArrayList<View>();
+        textViews = new ArrayList<TextView>();
+        degreeList = new ArrayList<Double>();
+        for (int i = 0; i < lists.size(); i++) {
+        	views.add(lists.get(i).view);
+			textViews.add(lists.get(i).textView);
+			degreeList.add(lists.get(i).degree);
+		}
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);  
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        //drawTexts(100);
+	}
+	@Override  
+    protected void onPause() {
+        super.onPause();  
+        sensorManager.unregisterListener(this);
+    }
+	@Override  
+    protected void onResume() {
+        super.onResume();  
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);  
+    }
+	
+	double dHeight, dWidth;
+	double coverDegree = 30;
+	private void drawTexts(double nowDirect) {
+		textsParent.removeAllViews();
+		/*TextView textView = new TextView(this);
+        //textView.setBackgroundResource(R.drawable.white_text_bg);
+        textView.setBackgroundColor(getResources().getColor(R.color.d500));
+        textView.setPadding(10, 10, 10, 10);
+        textView.setText("为什么画不出来？");
+        LayoutParams relativeParams  = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        relativeParams.topMargin = 100;
+		relativeParams.leftMargin = 100;
+		textView.setLayoutParams(relativeParams);
+		textsParent.addView(textView);*/
+		//给left加小范围random，不至于那么呆板
+		for (int i = 0; i < degreeList.size(); i++) {
+			double degree = degreeList.get(i);
+			View viewParent = views.get(i);
+			double delta = degree - nowDirect;
+			LayoutParams relativeParams = new RelativeLayout.LayoutParams(200, LayoutParams.WRAP_CONTENT);
+			if (delta >=0 && delta <= coverDegree) {//画在右边
+				relativeParams.topMargin = (int)(Math.random() * 400);
+				relativeParams.leftMargin = (int)(dWidth/2 + dWidth/2 * delta/coverDegree + Math.random()*100 - 50);
+				
+			} else if (delta >=-coverDegree && delta < 0) {//画左边
+				relativeParams.topMargin = (int)(Math.random() * 400);
+				relativeParams.leftMargin = (int)(dWidth/2 - dWidth/2 * Math.abs(delta)/coverDegree + Math.random()*100 - 50);
+			}
+			viewParent.setLayoutParams(relativeParams);
+			textsParent.addView(viewParent);
+		}
+		/*for (int i = 0; i < degreeList.size(); i++) {
+			double degree = degreeList.get(i);
+			TextView textView = new TextView(this);
+			textView = textViews.get(i);
+			double delta = degree - nowDirect;
+			LayoutParams relativeParams = new RelativeLayout.LayoutParams(200, LayoutParams.WRAP_CONTENT);
+			if (delta >=0 && delta <= coverDegree) {//画在右边
+				relativeParams.topMargin = (int)(Math.random() * 400);
+				relativeParams.leftMargin = (int)(dWidth/2 + dWidth/2 * delta/coverDegree);
+				textView.setLayoutParams(relativeParams);
+				textsParent.addView(textView);
+			} else if (delta >=-coverDegree && delta < 0) {//画左边
+				relativeParams.topMargin = (int)(Math.random() * 400);
+				relativeParams.leftMargin = (int)(dWidth/2 - dWidth/2 * Math.abs(delta)/coverDegree);
+				textView.setLayoutParams(relativeParams);
+				textsParent.addView(textView);
+			}
+			//judgeTextColor(relativeParams, textView, adList.get(i).getDistance());
+		}*/
+	}
+	
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		double directNow = event.values[0];
+		if (Math.abs(directNow - lastDirection) > 10) {//超过10°刷新一下
+			lastDirection = directNow;
+			drawTexts(directNow);
+		}
+	}
+
+	private Camera camera;
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+		camera = Camera.open();
+		Camera.Parameters param = camera.getParameters();
+		if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+			camera.setDisplayOrientation(90);
+		} else {
+			camera.setDisplayOrientation(0);
+		}
+		camera.setParameters(param);
+		try {
+			camera.setPreviewDisplay(holder);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		camera.startPreview();
+			
+	}
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width,
+			int height) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+		camera.stopPreview();
+		camera.release();
+	}
+
+}
---
+++ src/icy/nearad/TextFactory.java
@@ -0,0 +1,156 @@
+package icy.nearad;
+
+import icy.baixing.entity.Ad;
+import icy.baixing.entity.Ad.EDATAKEYS;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.zip.Inflater;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class TextFactory {
+    private ArrayList<Integer> distanceList;
+    private ArrayList<Ad> adList;
+    private DisComparator disComparator;
+    private Context context;
+
+    public TextFactory(Context context) {
+        disComparator = new DisComparator();
+        this.context = context;
+    }
+    
+    public ArrayList<ViewWithDegree> setAds(ArrayList<Ad> ads) {
+        this.adList = ads;
+        Collections.sort(adList, disComparator);//按距离排过序的ad
+        ArrayList<ViewWithDegree> list = generateText();
+        return list;
+    }
+    
+    public ArrayList<Ad> getAdList() {
+		return adList;
+	}
+
+    public class ViewWithDegree {
+    	double degree;
+    	TextView textView;
+    	View view;
+    }
+    public void draw(ArrayList<ViewWithDegree> lists, View parent) {
+		for (int i = 0; i < lists.size(); i++) {
+			
+		}
+	}
+    private ArrayList<ViewWithDegree> generateText() {
+        ArrayList<ViewWithDegree> dTextViews = new ArrayList<ViewWithDegree>();
+        for (int i=0; i<adList.size(); i++) {
+        	LinearLayout linearLayout = (LinearLayout)LayoutInflater.from(context).inflate(R.layout.item_textview, null);
+        	TextView textView = (TextView)linearLayout.findViewById(R.id.id_ad_title);
+        	//判断linearlayout的背景框,超过1500白线条,低于则黑线条
+        	//判断textColor同上
+        	//判断text的background，则要每隔500判断一个
+        	judgeTextColor(linearLayout, textView, adList.get(i).getDistance());
+            textView.setPadding(10, 10, 10, 10);
+            String string = String.valueOf(adList.get(i).getDistance()) + "\n";
+            textView.setText(string + adList.get(i).getValueByKey(EDATAKEYS.EDATAKEYS_TITLE));
+            ViewWithDegree viewWithDegree = new ViewWithDegree();
+            viewWithDegree.degree = calculateDegree(adList.get(i));
+            viewWithDegree.textView = textView;
+            viewWithDegree.view = (View) linearLayout;
+            dTextViews.add(viewWithDegree);
+        }
+        return dTextViews;
+    }
+    
+    private void judgeTextColor(LinearLayout linearLayout, TextView textView, double distance) {
+    	linearLayout.setBackgroundResource(R.drawable.white_text_bg);
+    	textView.setTextColor(context.getResources().getColor(R.color.white));
+    	if (distance <= 500) {
+    		textView.setBackgroundColor(0xee111111);//context.getResources().getColor(R.color.d500)
+    		return;
+        } else if (distance <= 800) {
+        	textView.setBackgroundColor(0xee181818);
+        	return;
+		} else if (distance <= 1000) {
+        	textView.setBackgroundColor(0xee222222);
+        	return;
+        } else if (distance <= 1200) {
+        	textView.setBackgroundColor(0xee292929);
+        	return;
+		} else if (distance <= 1500) {
+        	textView.setBackgroundColor(0xee333333);
+        	return;
+        } else if (distance <= 1800) {
+        	textView.setBackgroundColor(0xee393939);
+        	return;
+        }
+    	linearLayout.setBackgroundResource(R.drawable.black_text_bg);
+    	textView.setTextColor(context.getResources().getColor(R.color.black));
+    	if (distance <= 2000) {
+    		textView.setBackgroundColor(0xee444444);
+    		return;
+        } else if (distance <= 2300) {
+        	textView.setBackgroundColor(0xee484848);
+        	return;
+        } else if (distance <= 2500) {
+        	textView.setBackgroundColor(0xee515151);
+        	return;
+        } else {
+        	textView.setBackgroundColor(0xee555555);
+        	return;
+        }
+	}
+    private double calculateDegree(Ad ad) {
+    	double adLat = Double.parseDouble(ad.getValueByKey(EDATAKEYS.EDATAKEYS_LAT));
+		double adLon = Double.parseDouble(ad.getValueByKey(EDATAKEYS.EDATAKEYS_LON));
+		double degree = Math.atan2(adLon - SplashCover.getLng(), adLat - SplashCover.getLat()) * 180 / Math.PI;
+		//分三种情况
+		double adDegree;
+		if (degree <= 0) {
+			adDegree = 90 + Math.abs(degree);
+		} else if (degree <= 90 && degree > 0) {
+			adDegree = 90 - degree;
+		} else {
+			adDegree = 270 + (180 - degree);
+		}
+		return adDegree;//与北方的夹角,和传感器一致
+	}
+    
+    private int colorResource(double distance) {
+        if (distance <= 500) {
+            return context.getResources().getColor(R.color.d500);
+        } else if (distance <= 1000) {
+            return context.getResources().getColor(R.color.d1000);
+        } else if (distance <= 1500) {
+            return context.getResources().getColor(R.color.d1500);
+        } else if (distance <= 2000) {
+            return context.getResources().getColor(R.color.d2000);
+        } else if (distance <= 2500) {
+            return context.getResources().getColor(R.color.d2500);
+        } else {
+            return context.getResources().getColor(R.color.d3000); 
+        }
+    }
+    
+    private class DisComparator implements Comparator<Ad> {
+
+        @Override
+        public int compare(Ad lhs, Ad rhs) {
+            double dis1 = lhs.getDistance();
+            double dis2 = rhs.getDistance();
+            if (dis1 == dis2) {
+                return 0;
+            } else if (dis1 > dis2) {
+                return -1;
+            } else {
+                return 1;
+            }
+        }
+    }
+}
